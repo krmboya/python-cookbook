e734d889b9cefcb52d6a74650afe5ba883ff061e
Kenny Rachuonyo <krmboya@gmail.com>
2017-02-28 13:26:35 +0300

    please flake8

--- src/1/extracting_a_subset_of_a_dictionary/example.py
+++ src/1/extracting_a_subset_of_a_dictionary/example.py
@@ -10,14 +10,14 @@ prices = {
 }
 
 # Make a dictionary of all prices over 200
-p1 = { key:value for key, value in prices.items() if value > 200 }
+p1 = {key: value for key, value in prices.items() if value > 200}
 
 print("All prices over 200")
 pprint(p1)
 
 # Make a dictionary of tech stocks
-tech_names = { 'AAPL', 'IBM', 'HPQ', 'MSFT' }
-p2 = { key:value for key,value in prices.items() if key in tech_names }
+tech_names = {'AAPL', 'IBM', 'HPQ', 'MSFT'}
+p2 = {key: value for key, value in prices.items() if key in tech_names}
 
 print("All techs")
 pprint(p2)
